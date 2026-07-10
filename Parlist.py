117d586c6aef12fe417140a435e8f7c229a8e0b8
amihatovic07 <alexmihatovic@gmail.com>
2026-07-10 23:34:36 +0200

    Commit 2.
    Unutar ovog commita je pridodan popravljen algoritam koji će naknadno biti nadograđen sa matričnim računanjem, tabličnim zapisom i grafičkom vizualizacijom radi detaljnosti i boljeg korisničkog ugođaja

--- Parlist.py
+++ Parlist.py
@@ -8,9 +8,7 @@
 
 # Prvo se uvode sve potrebne biblioteke i moduli koji će se koristiti unutar aplikacije
 
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
+import sys, numpy as np, pandas as pd, matplotlib.pyplot as plt
 
 # Globalno varijabilna polja i varijable
 
@@ -34,23 +32,24 @@ def napomena():
 # Funkcija za izbornik
 
 def izbornik():
-    print("1. prihod")
-    print("2. rashod")
-    print("3. stanje poslovanja")
-    print("4. plan poslovanja")
-    print("5. izlazak iz sustava")
+    print("1. unos prihoda i rashoda")
+    print("2. stanje poslovanja")
+    print("3. plan poslovanja")
+    print("4. izlazak iz sustava")
+    print("5. napomena")
+    print(" ")
     
 # Funkcija za unos mjesečnog prihoda i rashoda
 
 def unos(mjeseci, prihodi, rashodi):
     print("Unesite prihode za svaki mjesec\n-------------------------")
     for i in range(len(mjeseci)):
-        x = int(input(f"{i}: "))
+        x = int(input(f"{i+1}: "))
         prihodi.append(x)
     print("Unesite rashode za svaki mjesec\n-------------------------")
     for i in range(len(mjeseci)):
-        y = int(input(f"{i}: "))
-        rashodi.append(x)
+        y = int(input(f"{i+1}: "))
+        rashodi.append(y)
     return prihodi, rashodi
 
 # Funkcija za provjeru stanja nakon izjednačavanja prihoda i rashoda
@@ -66,10 +65,53 @@ def provjera_stanja(prihodi, rashodi, lose_stanje):
         lose_stanje = False
         return "U dobrom je stanju poslovanje!", lose_stanje
 
+# Funkcija koja formira stanje na temelju prihoda i rashoda u formatu jedne godine
+
+def stanje(prihodi, rashodi):
+    x = sum(prihodi)
+    y = sum(rashodi)
+    stanje = x - y
+    return stanje
+
+# Funkcija za unos plana poslovanja
+
+def plan_poslovanja(prihodi, rashodi, lose_stanje):
+    trenutno_stanje = stanje(prihodi, rashodi)
+    neostvareno = False
+    negativa = False
+    plan_stanje = int(input("Unesite vaše planirano stanje: "))
+    if trenutno_stanje < plan_stanje:
+        neostvareno = True
+    if (lose_stanje):
+        negativa = True
+    if (neostvareno and negativa):
+        return "Kako biste ostvarili željeno stanje potrebno je da smanjite rashode!"
+    elif (neostvareno and not negativa):
+        return "Kako biste ostvarili željeno stanje potrebno je povečati prihode!"
+    elif (not neostvareno and not negativa):
+        return "Ostvarili ste već željeno stanje!"
+    else:
+        return "Nevažeći unos!"
+
+
+
 # Glavna funkcija koja pokreće cijelu aplikaciju čim je pokrenuta
 
 def main(mjeseci, prihodi, rashodi, lose_stanje):
-    napomena()
-    unos(mjeseci, prihodi, rashodi)
-    provjera_stanja(prihodi, rashodi, lose_stanje)
+    while True:
+        napomena()
+        while True:
+            izbornik()
+            odg = int(input("Unesite broj funkcionalnosti (1, 2, 3, 4 ili 5): "))
+            if odg == 1:
+                unos(mjeseci, prihodi, rashodi)
+            elif odg == 2:
+                provjera_stanja(prihodi, rashodi, lose_stanje)
+            elif odg == 3:
+                plan_poslovanja(stanje(prihodi, rashodi), provjera_stanja(prihodi, rashodi, lose_stanje))
+            elif odg == 4:
+                sys.exit()
+            elif odg == 5:
+                break
+
 main(mjeseci, prihodi, rashodi, lose_stanje)
